0f60c3f6c4bc784d88c823c2b88277bf85346c03
ellechoo <michellehchoo@gmail.com>
2024-11-24 07:41:36 +0000

    10css

--- sketch.js
+++
@@ -1,9 +0,0 @@
-function setup() {
-    createCanvas(100, 100);
-  
-    background(200);
-  
-    rect(30, 20, 55, 55);
-  
-    describe('A white square with a black outline on a gray canvas.');
-  }
